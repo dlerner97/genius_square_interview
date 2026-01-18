7efaf974f5b42c0e2d9b14dfcba24e185758754d
Dani Lerner <dlerner97@gmail.com>
2026-01-18 13:07:16 -0500

    Created grid and blockers

---
+++ src/board_utils.rs
@@ -0,0 +1,6 @@
+
+
+pub const GRID_SIZE: (usize, usize) = (6, 6);
+pub const X_EXTENT: f32 = 1000.;
+pub const Y_EXTENT: f32 = 150.;
+pub const THICKNESS: f32 = 5.0;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod pieces;
+mod bevy_impl;
+mod board_utils;
+
 use ndarray::Array2;
-use bevy::prelude::*;
+use bevy_impl::setup;
+
+use bevy::{
+    input::common_conditions::input_just_pressed,
+    sprite_render::{Wireframe2dConfig, Wireframe2dPlugin},
+};
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
 
 fn arr2str(arr: &Array2<String>) -> String {
     let (rows, cols) = arr.dim();
@@ -18,6 +27,29 @@ fn arr2str(arr: &Array2<String>) -> String {
 }
 
 fn main() {
-    // App::new().run();
+    let mut app = App::new();
+    app.add_plugins((
+        DefaultPlugins,
+        Wireframe2dPlugin::default(),
+    ))
+    .add_systems(Startup, setup);
+    app.add_systems(
+        Update,
+        toggle_wireframe.run_if(input_just_pressed(KeyCode::Space)),
+    );
+    app.add_systems(
+        Update,
+        rotate.run_if(input_toggle_active(false, KeyCode::KeyR)),
+    );
+    app.run();
+}
 
+fn toggle_wireframe(mut wireframe_config: ResMut<Wireframe2dConfig>) {
+    wireframe_config.global = !wireframe_config.global;
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Mesh2d>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_z(time.delta_secs() / 2.0);
+    }
 }
